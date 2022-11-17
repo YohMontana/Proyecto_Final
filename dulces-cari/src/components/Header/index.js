6a371c4192b2f1f223d3cfb593f373315deaf3db
Yohmontana <estebanjcarlo@gmail.com>
2022-11-16 23:34:37 -0500

    intento julio

--- dulces-cari/src/components/Header/index.js
+++ dulces-cari/src/components/Header/index.js
@@ -22,7 +22,6 @@ function Header() {
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav ml-auto">
                             <li className="nav-item"><a className="nav-link active" aria-current="page" href="#">Inicio</a></li>
-                            <li className="nav-item"><a className="nav-link" href="#">Inicio</a></li>
                             <li className="nav-item"><a className="nav-link" href="#">Nosotros</a></li>
                             <li className="nav-item"><a className="nav-link" href="#">Tienda</a></li>
                             <li className="nav-item"><a className="nav-link" href="#">Contactanos</a></li>
