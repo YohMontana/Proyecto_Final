26bc3e6df3e26ffa8d0ab237d125a174189a0793
Lisseth Delgadillo <lisseth.delgadillo.huaman@gmail.com>
2022-11-17 00:27:54 -0500

    primera parte

--- dulces-cari/src/App.js
+++ dulces-cari/src/App.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-import Footer from './components/footer/Footer';
-import Preguntas from './components/pages/Preguntas';
-import Reclamaciones from './components/pages/Reclamaciones';
-import Terminos from './components/pages/Terminos';
+import Footer from './Components/footer/Footer'
+import Preguntas from './Components/pages/Preguntas'
+import Reclamaciones from './Components/pages/Reclamaciones'
+import Terminos from './Components/pages/Terminos'
 import './App.css';
 
 function App() {
   return (
-   
+    <div>
       <Router>
         
           <Footer/>
       
        
       </Router>
-      
+    </div>
     
 
    
