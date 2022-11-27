6dc5d6ffb00f7a560c7a68bba97aae524057fd8d
Elvia Madrid <elvia.madrid97@gmail.com>
2022-11-27 04:03:02 -0500

    consolidado ramas

--- src 26nov 11pm/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as HomeView } from "./Home";
-export { default as AboutView } from "./About";
-export { default as ProductView} from "./products";
-export { default as Product} from "./Product";
