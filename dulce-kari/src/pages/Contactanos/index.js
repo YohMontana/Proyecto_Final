b98c339cb41333ff2201b3f483376e29a7fa5680
Yohmontana <estebanjcarlo@gmail.com>
2022-12-01 09:16:50 -0500

    avance

--- dulce-kari/src/pages/Contactanos/index.js
+++ dulce-kari/src/pages/Contactanos/index.js
@@ -1,11 +1,7 @@
 import { Mailer } from '../../components' 
 import { Encuentranos } from '../../components'
 import { CardFooter } from '../../components';
-<<<<<<< HEAD
-
-=======
 import {Division} from '../../components';
->>>>>>> 02fe2bfaf5cab3046016da90cba98e913f8c0361
 
 
 function Contactanos () {
@@ -13,16 +9,10 @@ function Contactanos () {
     <>
       <Mailer></Mailer>     
       <Encuentranos></Encuentranos>
-<<<<<<< HEAD
-      <CardFooter></CardFooter>
-    </> 
-    
-=======
       <Division></Division>
       <CardFooter></CardFooter>
     </> 
 
->>>>>>> 02fe2bfaf5cab3046016da90cba98e913f8c0361
   );
 }
 
